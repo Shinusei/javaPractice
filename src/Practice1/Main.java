adb4669be4ffe8959d6ca53a7cf695818d1596b7
Shinusei <lyshagrishin@gmail.com>
2023-09-05 10:21:02 +0300

    create

--- src/Practice1/Main.java
+++ src/Practice1/Main.java
@@ -1,19 +1,27 @@
 package Practice1;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        short arrSize = 5;
+        int[] arr = new int[5];
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        for(int i = 0; i < arrSize; i++) {
+            if (input.hasNextInt()) {
+                arr[i] = input.nextInt();
+            } else {
+                System.out.println("Not digit");
+                System.exit(0);
+            }
         }
+        int arrSum = 0;
+        double arrMid = 0;
+        for(int i : arr){
+            arrSum += i;
+        }
+        System.out.println(arrSum);
+
     }
 }
