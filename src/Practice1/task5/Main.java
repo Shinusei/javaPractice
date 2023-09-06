a4fd5afba2f3452194b4c3c506a292964fde51f8
Shinusei <lyshagrishin@gmail.com>
2023-09-06 17:05:07 +0300

    create

---
+++ src/Practice1/task5/Main.java
@@ -0,0 +1,9 @@
+package Practice1.task5;
+
+public class Main {
+    public static void main(String[] args) {
+        for (String i : args){
+            System.out.println(i);
+        }
+    }
+}
