e0c9e8913f173abfdd704b036ac8756a2f67dd78
Shinusei <lyshagrishin@gmail.com>
2023-09-07 10:19:22 +0300

    Practice

---
+++ src/Practice2/task1/TestAuthor.java
@@ -0,0 +1,15 @@
+package Practice2.task1;
+
+import java.util.Scanner;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Author Puskin = new Author("Alexander Pushkin", "pushkin@mail.ru", 'm');
+        System.out.println(Puskin.toString());
+        System.out.print("new email: ");
+        String email = scanner.next();
+        Puskin.setEmail(email);
+        System.out.println(Puskin.toString());
+    }
+}
