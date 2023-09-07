9a3f0f0c1c188c6af3360f7b79c3e944acf4ba64
Shinusei <lyshagrishin@gmail.com>
2023-09-07 09:50:37 +0300

    Practice

--- src/Practice1/task3/Main.java
+++ src/Practice1/task3/Main.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        short arrSize = 5;
-        int[] arr = new int[5];
+        int[] arr = new int[]{1, 4, 3, 6, 8};
         int arrSum = 0;
-        for(int i = 0; i < arrSize; i++) {
-            arr[i]=i;
+        for(int i : arr) {
             arrSum+=i;
         }
-        double arrMid = (double) arrSum /arrSize;
+        double arrMid = (double) arrSum /arr.length;
         System.out.println(arrSum+" "+arrMid);
     }
 }
