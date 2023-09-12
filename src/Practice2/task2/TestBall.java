994e65c9af06a5292beed2aaee9d93122d3d68e5
Shinusei <lyshagrishin@gmail.com>
2023-09-12 10:17:59 +0300

    Practice

---
+++ src/Practice2/task2/TestBall.java
@@ -0,0 +1,14 @@
+package Practice2.task2;
+
+import java.util.Scanner;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Ball ball = new Ball(4, 7);
+        System.out.println(ball);
+        System.out.print("new cords: ");
+        ball.move(scanner.nextInt(), scanner.nextInt());
+        System.out.println(ball);
+    }
+}
