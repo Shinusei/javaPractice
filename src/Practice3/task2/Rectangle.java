e1f8b56da148bfe4b146ec481368d1be04c500f6
Shinusei <lyshagrishin@gmail.com>
2023-09-14 09:50:08 +0300

    Practice

---
+++ src/Practice3/task2/Rectangle.java
@@ -0,0 +1,56 @@
+package Practice3.task2;
+
+public class Rectangle extends Shape{
+    protected  double width;
+    protected double length;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    double getArea() {
+        return width*length;
+    }
+
+    @Override
+    double getPerimetr() {
+        return 2*(width+length);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ src/Practice3/task2/Shape.java
@@ -0,0 +1,33 @@
+package Practice3.task2;
+
+public abstract class Shape {
+    protected String color;
+    protected boolean filled;
+
+    public Shape() {
+    }
+
+    public Shape(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+    abstract double getArea();
+    abstract double getPerimetr();
+    public abstract String toString();
+}
