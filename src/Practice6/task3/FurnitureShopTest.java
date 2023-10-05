89e1b77eb9b70eaaed1f168c9f2a4ade09895dd3
shinusei <lyshagrishin@gmail.com>
2023-10-05 10:06:55 +0300

    final prac6

---
+++ src/Practice6/task3/FurnitureShopTest.java
@@ -0,0 +1,13 @@
+package Practice6.task3;
+
+public class FurnitureShopTest {
+    public static void main(String[] args) {
+        FurnitureShop shop = new FurnitureShop(5);
+        shop.add(new Table(
+                "Металлический стол", 2, 1.3, 2, 2, 5930, "Металл+стекло", 0
+        ));
+        System.out.println(shop.getPrices());
+        System.out.println("Куплено: "+ shop.buy(3));
+        System.out.println(shop.getPrices());
+    }
+}
