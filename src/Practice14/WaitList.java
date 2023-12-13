db5eeebb213ed21dafd8e470b860dcced3285453
aleksey <lyshagrishin@gmail.com>
2023-12-13 21:57:32 +0300

    litle changes for the better

--- src/Practice14/WaitList.java
+++ src/Practice14/WaitList.java
@@ -3,7 +3,7 @@ package Practice14;
 import java.util.Collection;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-public class WaitList<E> extends IWaitList<E> {
+public class WaitList<E> implements IWaitList<E> {
     protected ConcurrentLinkedQueue<E> content;
 
     /**
